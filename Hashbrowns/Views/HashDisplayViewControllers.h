f1dc92031b2989ccfeef481ed66ca29710bc903f
admin <admin@admins-MacBook-Pro.local>
2017-11-17 22:10:26 -0800

    Added new view controller with segue from input/form view

---
+++ Hashbrowns/Views/HashDisplayViewControllers.h
@@ -0,0 +1,13 @@
+//
+//  HashDisplayViewControllers.h
+//  Hashbrowns
+//
+//  Created by admin on 11/17/17.
+//  Copyright © 2017 jml. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HashDisplayViewControllers : NSViewController
+
+@end
---
+++ Hashbrowns/Views/HashDisplayViewControllers.m
@@ -0,0 +1,22 @@
+//
+//  HashDisplayViewControllers.m
+//  Hashbrowns
+//
+//  Created by admin on 11/17/17.
+//  Copyright © 2017 jml. All rights reserved.
+//
+
+#import "HashDisplayViewControllers.h"
+
+@interface HashDisplayViewControllers ()
+
+@end
+
+@implementation HashDisplayViewControllers
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
